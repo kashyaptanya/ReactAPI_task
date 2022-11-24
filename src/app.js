fe2c71a4b15a87aedd2c4acff01926c7ac7b0f78
Tanya Kashyap <kashyaptanya105@gmail.com>
2022-11-25 00:33:01 +0530

    setup react project without using CRA

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './header'
+const App = () => {
+  return (
+<>
+    <h1>   Create React App Without Command</h1>
+   <Header/>
+    </>
+
+
+  )
+}
+
+export default App
